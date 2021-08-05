36ca8911f100302da4812a3986e7d6ae0d340ce9
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-08-05 04:14:25 -0300

    feat: create swipeable item to mobile version

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useRef, useState, TouchEvent, HTMLAttributes, useCallback } from 'react';
+import { Trash } from 'react-feather';
+
+import { useTransactions } from '../../hooks/useTransactions';
+
 import {
   Container
 } from './styles';
@@ -12,24 +17,142 @@ interface Transaction {
 }
 
 interface ListItemProps {
-  transaction: Transaction
+  transaction: Transaction;
 }
 
 export function ListItem({ transaction }: ListItemProps) {
-  return (
-    <Container onClick={() => console.log('asd')}>
-      <div className="header">
-        <p>{transaction.title}</p>
-        <strong
-          className={transaction.type}
-        >
-          {transaction.amount}
-        </strong>
-      </div>
+  const containerRef = useRef<HTMLLIElement>(null);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0)
+  const [containerStyle, setContainerStyle] = useState<HTMLAttributes<HTMLDivElement>['style']>({ left: '0px' });
+  const [touchMoveX, setTouchMoveX] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [swipeActionTriggered, setSwipeActionTriggered] = useState(false);
 
-      <div className="footer">
-        <p>{transaction.category}</p>
-        <p>{transaction.createdAt}</p>
+  const { deleteTransaction } = useTransactions();
+
+  function askDeleteTransaction() {
+    return window.confirm('Deseja mesmo apagar esse registro?');
+  }
+
+  function handleStart(clientX: number) {
+    setTouchStartX(clientX);
+    setIsDragging(true);
+  }
+
+  function handleMove(clientX: number) {
+    if (isDragging) {
+      setTouchMoveX(clientX);
+      let deltaX = clientX - touchStartX;
+      const twoThirdsWidth = (containerWidth / 3) * 2;
+
+      if (deltaX <= -(twoThirdsWidth)) {
+        setIsDragging(false);
+        setSwipeActionTriggered(true);
+        const shouldDeleteTransaction = askDeleteTransaction();
+
+        if (shouldDeleteTransaction) {
+          deleteTransaction(transaction.id);
+        } else {
+          setTouchMoveX(0);
+          setSwipeActionTriggered(false);
+          setContainerStyle({ left: '0px', transition: 'ease-out 0.2s' });
+          return;
+        }
+      } else if (deltaX > 0) {
+        deltaX = 0;
+      }
+      setContainerStyle({ left: `${deltaX}px` });
+    }
+  }
+
+  function handleEnd() {
+    setIsDragging(false);
+    const deltaX = touchMoveX - touchStartX;
+    const halfTheMiddleWidth = (containerWidth / 2);
+    const twoThirdsWidth = (containerWidth / 3) * 2;
+
+    if (
+      deltaX <= -(halfTheMiddleWidth)
+      && !swipeActionTriggered
+      && touchMoveX !== 0
+    ) {
+      setContainerStyle(() => ({ left: `${-twoThirdsWidth}px`, transition: 'ease-out 0.2s' }));
+      let shouldDeleteTransaction: boolean;
+
+      setTimeout(() => {
+        shouldDeleteTransaction = askDeleteTransaction();
+
+        if (shouldDeleteTransaction) {
+          deleteTransaction(transaction.id);
+        } else {
+          setTouchMoveX(0);
+          setContainerStyle({ left: '0px', transition: 'ease-out 0.4s' });
+        }
+      }, 300);
+      return;
+    }
+
+    setContainerStyle({ left: '0px', transition: 'ease-out 0.2s' });
+  }
+
+  const handleTouchStart = useCallback((touchStartEvent: globalThis.TouchEvent) => {
+    touchStartEvent.preventDefault();
+    handleStart(touchStartEvent.targetTouches[0].clientX);
+  }, [])
+
+  function handleTouchMove(touchMoveEvent: TouchEvent<HTMLLIElement>) {
+    handleMove(touchMoveEvent.targetTouches[0].clientX);
+  }
+
+  function handleTouchEnd() {
+    handleEnd();
+  }
+
+  useEffect(() => {
+    const containerRefValue = containerRef.current;
+
+    if (containerRefValue?.offsetWidth) {
+      setContainerWidth(containerRefValue.offsetWidth);
+    }
+
+    containerRefValue?.addEventListener(
+      'touchstart',
+      handleTouchStart,
+    );
+
+    return () => {
+      containerRefValue?.removeEventListener(
+        'touchstart',
+        handleTouchStart,
+      );
+    }
+  }, [handleTouchStart])
+
+  return (
+    <Container
+      ref={containerRef}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      <Trash color="#FFF" size={18} />
+      <div
+        className="swipeable-content"
+        style={containerStyle}
+      >
+        <div className="header">
+          <p>{transaction.title}</p>
+          <strong
+            className={transaction.type}
+          >
+            {transaction.amount}
+          </strong>
+        </div>
+
+        <div className="footer">
+          <p>{transaction.category}</p>
+          <p>{transaction.createdAt}</p>
+        </div>
       </div>
     </Container>
   );
--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,56 +1,62 @@
 import styled from 'styled-components'
 
 export const Container = styled.li`
-  display: flex;
-  flex-direction: column;
-  background: var(--shape);
-  padding: 1.05rem 1.5rem;
+  position: relative;
+  height: 8rem;
+  background: var(--red);
   border-radius: 0.3rem;
-  width: 100%;
-  
-  /* button {
-    border: 0;
-    background: transparent;
-    font-size: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  svg {
+    margin-right: 7.3rem;
+  }
+
+  & + li {
+    margin-top: 0.5rem;
+  }
+
+  div.swipeable-content {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+
     display: flex;
     flex-direction: column;
     background: var(--shape);
     padding: 1.05rem 1.5rem;
     border-radius: 0.3rem;
     width: 100%;
-  } */
 
-  & + li {
-    margin-top: 0.5rem;
-  }
+    div.header {
+      p {
+        color: var(--text-title);
+        font-size: 0.875rem;
+      }
 
-  div.header {
-    p {
-      color: var(--text-title);
+      strong {
+        display: block;
+        margin-top: 0.125rem;
+        font-size: 1.25rem;
+
+        &.deposit {
+          color: #12a454;
+        }
+
+        &.withdraw {
+          color: var(--red);
+        }
+      }
+    }
+
+    div.footer {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin-top: 1.18rem;
+      color: var(--text-body);
       font-size: 0.875rem;
     }
-
-    strong {
-      display: block;
-      margin-top: 0.125rem;
-      font-size: 1.25rem;
-
-      &.deposit {
-        color: #12a454;
-      }
-
-      &.withdraw {
-        color: var(--red);
-      }
-    }
-  }
-
-  div.footer {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    margin-top: 1.18rem;
-    color: var(--text-body);
-    font-size: 0.875rem;
   }
 `;
