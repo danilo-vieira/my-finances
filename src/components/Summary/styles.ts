e6f9cb1544123a4c30922a7ea2356298bcfc4dc6
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-08-05 22:40:06 -0300

    chore: fix summary card size

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -6,7 +6,13 @@ export const Container = styled.div`
   gap: 2rem;
   margin-top: -4.25rem;
 
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
   div {
+    width: 22rem;
     background: var(--shape);
     padding: 1.5rem 2rem;
     border-radius: 0.3rem;
@@ -64,11 +70,6 @@ export const Container = styled.div`
           color: var(--shape);
         }
       }
-    }
-
-    overflow-x: scroll;
-    &::-webkit-scrollbar {
-      display: none;
     }
   }
 `;
