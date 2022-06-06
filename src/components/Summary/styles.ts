6b56f4023854f6036b3f38fb7b474821f56f3b94
Danilo Vieira <danilo_strvieira@hotmail.com>
2022-06-06 20:57:08 -0300

    changes unexpected overflow hidden in total card

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 2rem;
+  gap: 1rem;
   margin-top: -4.25rem;
 
   overflow-x: scroll;
